5859cac5eadaa54ff678386255b8d89bbb6e1448
unknown <gokaytemizkan@gmail.com>
2019-10-09 17:25:15 +0300

    server.py is returned to original form, modified version is kept as servermodified.py

--- server.py
+++ server.py
@@ -1,18 +1,13 @@
 from flask import Flask
 
-import views
+
+app = Flask(__name__)
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object("settings")
-
-    app.add_url_rule("/", view_func=views.home_page)
-
-    return app
+@app.route("/")
+def home_page():
+    return "Hello, world!"
 
 
 if __name__ == "__main__":
-    app = create_app()
-    port = app.config.get("PORT", 8080)
-    app.run(host="0.0.0.0", port=port)
+    app.run()
