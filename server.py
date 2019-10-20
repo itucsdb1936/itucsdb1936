4f382300748470a0d62a6a2e7b3355f333af67a6
gokaytem <gokaytemizkan@gmail.com>
2019-10-20 23:33:01 +0300

    Unnecessary comments removed from server.py

--- server.py
+++ server.py
@@ -33,12 +33,6 @@ def initialize(url):
             cursor.execute(statement)
         cursor.close()
 
-#import os
-#import psycopg2
-
-#from psycopg2 import sql
-
-#from sqlalchemy import create_engine
 
 from datetime import datetime
 
@@ -46,53 +40,7 @@ from datetime import datetime
 
 app=Flask(__name__)
 
-#db_user="postgres"
-#db_password="docker"
-#host="localhost"
-#port="5432"
-#db_name="postgres"
 
-#engine_params = (f'postgresql+psycopg2://{db_user}:{db_password}@{host}:{port}/{db_name}')
-
-#engine = create_engine(engine_params)
-#conn = engine.raw_connection()
-#cur = conn.cursor()
-
-#DATABASE_URL = os.environ['postgres://postgres:docker@localhost:5432/postgres']
-#connection = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-#DATABASE_URL="postgres://postgres:docker@localhost:5432/postgres"
-#connection = psycopg2.connect(user = "postgres",
-#                                  password = "docker",
-#                                  host = "localhost",
-#                                  port = "5432",
-#                                  database = "postgres",
-#                                  sslmode='require')
-
-#cursor = connection.cursor()
-
-#create_table_query = '''CREATE TABLE mobile
-#          (ID INT PRIMARY KEY     NOT NULL,
-#          MODEL           TEXT    NOT NULL,
-#          PRICE         REAL); '''
-#    
-#cursor.execute(create_table_query)
-#connection.commit()
-#print("Table created successfully in PostgreSQL ")
-#
-#    # Print PostgreSQL Connection properties
-#print ( connection.get_dsn_parameters(),"\n")
-#
-#    # Print PostgreSQL version
-#cursor.execute("SELECT version();")
-#record = cursor.fetchone()
-#print("You are connected to - ", record,"\n")
-#
-#    #closing database connection.
-#if(connection):
-#    cursor.close()
-#    connection.close()
-#    print("PostgreSQL connection is closed")
 
 @app.route("/")
 def home_page():
