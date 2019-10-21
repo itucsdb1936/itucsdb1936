97402844a55385b0f5faa8236b98193a9f90fe24
gokaytem <gokaytemizkan@gmail.com>
2019-10-21 15:34:39 +0300

    Fix bug attempt

--- dbinit.py
+++ dbinit.py
@@ -22,7 +22,7 @@ def initialize(url):
 
 if __name__ == "__main__":
     url = os.getenv("DATABASE_URL")
-#    if url is None:
-#        print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
-#        sys.exit(1)
+    if url is None:
+        print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
+        sys.exit(1)
     initialize(url)
