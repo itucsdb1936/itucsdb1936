4a58ac492f0544af409c40b1b35b4486aa9c7f09
gokaytem <gokaytemizkan@gmail.com>
2019-11-19 09:12:14 +0300

    Update method and page for Meetings table added

--- server.py
+++ server.py
@@ -132,7 +132,82 @@ def meetings_remove_page():
             cursor.close()
         
         return redirect(url_for("meetings_page"))
+
+@app.route("/meetings_update", methods=["GET", "POST"])
+def meetings_update_find_page():
+    if request.method == "GET":
+        return render_template(
+            "meeting_update_find.html"
+        )
+    else:
+        form_id = request.form["id"]
+        
+#        STATEMENTS = [ '''
+#                      SELECT * FROM MEETINGS
+#                          WHERE (ID=%s); ''' % (form_id)  ]
+#            
+#        url= "postgres://gvoybackrspqkf:339af7eacd4af135d7f93ef0df5dd3e25623e2a68da06335f5dc75855628fe95@ec2-54-247-171-30.eu-west-1.compute.amazonaws.com:5432/d7iva2beg4i1l0"
+#        with dbapi2.connect(url) as connection:
+#            cursor = connection.cursor()
+#            for statement in STATEMENTS:
+#                cursor.execute(statement)
+#                
+#            row = cursor.fetchone()
+            
+#            form_id = row[0]
+#            form_placeid = row[1]
+#            form_statusid = row[2]
+#            form_date = row[3]
+#            form_time = row[4]
+#            form_duration = row[5]
+#            form_topic = row[6]
+#            form_result = row[7]
+#            
+#            cursor.close()
+        
+        return redirect(url_for("meetings_update_change_page", id=form_id))
     
+@app.route("/meetings_update_<int:id>", methods=["GET", "POST"])
+def meetings_update_change_page(id):
+    if request.method == "GET":
+        STATEMENTS = [ '''
+                      SELECT * FROM MEETINGS
+                          WHERE (ID=%s); ''' % (id)  ]
+            
+        url= "postgres://gvoybackrspqkf:339af7eacd4af135d7f93ef0df5dd3e25623e2a68da06335f5dc75855628fe95@ec2-54-247-171-30.eu-west-1.compute.amazonaws.com:5432/d7iva2beg4i1l0"
+        with dbapi2.connect(url) as connection:
+            cursor = connection.cursor()
+            for statement in STATEMENTS:
+                cursor.execute(statement)
+                
+            row = cursor.fetchone()
+        return render_template(
+            "meeting_update_change.html", row=row
+        )
+    else:
+        form_id = request.form["id"]
+        form_placeid = request.form["place_id"]
+        form_statusid = request.form["status_id"]
+        form_date = request.form["date"]
+        form_time = request.form["time"]
+        form_duration = request.form["duration"]
+        form_topic = request.form["topic"]
+        form_result = request.form["result"]
+        
+        STATEMENTS = [ '''
+                      UPDATE MEETINGS
+                          SET ID=%s, PlaceID=%s, StatusID=%s, DATE='%s', TIME='%s', Duration='%s', Topic='%s', RESULT='%s'
+                          WHERE ID=%s; ''' % (form_id, form_placeid, form_statusid, form_date, form_time, form_duration, form_topic, form_result, form_id)  ]
+        
+        url= "postgres://gvoybackrspqkf:339af7eacd4af135d7f93ef0df5dd3e25623e2a68da06335f5dc75855628fe95@ec2-54-247-171-30.eu-west-1.compute.amazonaws.com:5432/d7iva2beg4i1l0"
+        with dbapi2.connect(url) as connection:
+            cursor = connection.cursor()
+            for statement in STATEMENTS:
+                cursor.execute(statement)
+        
+            cursor.close()
+        
+        return redirect(url_for("meetings_page"))
 
 #def create_app():
 #    app = Flask(__name__)
